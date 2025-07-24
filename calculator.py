fca39da58a1872a60c9a554124f52a7226de05f0
Patrickastro <apodopatrick@gmail.com>
2025-07-24 14:21:12 +0300

    PLP Intro to Python Assignment

---
+++ calculator.py
@@ -0,0 +1,4 @@
+Length = eval(input("Enter the length of your shape = "))
+widht = eval(input("Enter the width of your shape = "))
+circum = (Length*2) + widht*22/7
+print("The circumference of your shape is" ,circum)
