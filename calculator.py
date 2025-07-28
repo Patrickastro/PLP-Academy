36aa749f9921105b5925ba7e1b314fb2d6387f20
Patrickastro <apodopatrick@gmail.com>
2025-07-28 17:25:08 +0300

    Python Assignment 1 - Calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,19 @@
-Length = eval(input("Enter the length of your shape = "))
-widht = eval(input("Enter the width of your shape = "))
-circum = (Length*2) + widht*22/7
-print("The circumference of your shape is" ,circum)
+print("Input two numbers and an operator (e.g +, -, / and *) to explore this basic calculator")
+num1 = eval(input("Enter your first number : "))
+num2 = eval(input("Enter your second number : "))
+ch = input("Enter any of these operators [+, -, / or *] ")
+if ch == "/":
+        if num2 < 1:
+            print ("The denominator must be greater than zero")
+        else:
+            print("The division of the two numbers is", num1/num2)
+elif ch =="+":
+    print("The sum of the two numbers is", num1+num2)
+    
+elif ch == "-":
+    print("The difference between the two numbers is", num1-num2)
+    
+elif ch =="*":
+    print("The product of the two numbers is", num1*num2)
+else:
+    print("Enter an appropriate operator")
