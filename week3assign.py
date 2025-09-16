c486b0518a69dc83bd3d289f3934c69f393381e9
Patrickastro <apodopatrick@gmail.com>
2025-09-16 14:57:50 +0300

    modified:   week3assign.py

--- week3assign.py
+++ week3assign.py
@@ -1,4 +1,10 @@
-for number in range (1, 11): 
-    while number % 2 == 0:
-     
-     print(number)
+price = int(input("Enter the price of the item: "))
+discount = int(input("Enter the discount percentage (0-100): "))
+def calculate_discount(price, discount):
+    if discount >=20:
+        final_price = price * (1 - discount / 100)
+        print (f"The price of after applying discount is: {final_price}")
+    else:
+        print(f"Discount not applicable the final price is: {price}")
+calculate_discount(price, discount)
+        
